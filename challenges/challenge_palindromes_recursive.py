09365b065b8dec3530f405adb102d15b5147f528
Wadson92 <wadson.dev92@gmail.com>
2022-04-20 18:30:19 -0300

    feat. 2 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    try:
+        if word == "":
+            raise TypeError
+        if high_index == 0:
+            return True
+        else:
+            low_and_high = word[low_index] == word[high_index]
+            func = is_palindrome_recursive(word, low_index + 1, high_index - 1)
+            return low_and_high and func
+    except TypeError:
+        return False
