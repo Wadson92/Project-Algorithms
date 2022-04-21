c8d947a39eb50e9e58f4ebc1e68fa9dbc7d42990
Wadson92 <wadson.dev92@gmail.com>
2022-04-21 10:42:48 -0300

    fix requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,48 +1,50 @@
 def sort(array):
+    return sort_half(array, 0, len(array) - 1)
+
+
+def sort_half(array, start, end):
+    if start < end:
+        middle = (start + end) // 2
+
+        sort_half(array, start, middle)
+        sort_half(array, middle + 1, end)
+
+        merge(array, start, middle, end)
+
+
+def merge(array, start, middle, end):
     aux = array[:]
-    sort_half(array, aux, 0, len(array) - 1)
-
-
-def sort_half(array, aux, start, end):
-    if start >= end:
-        return
-    middle = (start + end) // 2
-
-    sort_half(array, aux, start, middle)
-    sort_half(array, aux, middle + 1, end)
-
-    merge(array, aux, start, end)
-
-
-def merge(array, aux, start, end):
     left = start
-    left_end = (start + end) // 2
+    left_end = middle + 1
+    arr = start
 
-    right = left_end + 1
-    right_end = end
-
-    for position in range(start, end + 1):
+    while arr <= end:
         if left > left_end:
-            aux[position] = array[right]
-            right += 1
+            array[arr] = aux[left_end]
+            left_end += 1
 
-        elif right > right_end:
-            aux[position] = array[left]
+        elif left_end > end:
+            array[arr] = aux[left]
             left += 1
 
-        elif array[left] < array[right]:
-            aux[position] = array[left]
+        elif aux[left] <= aux[left_end]:
+            array[arr] = aux[left]
             left += 1
 
         else:
-            aux[position] = array[right]
-            right += 1
+            array[arr] = aux[left_end]
+            left_end += 1
 
-        start += 1
+        arr += 1
 
 
 def is_anagram(first_string, second_string):
     try:
-        sort(first_string)
+        print(sort([2, 3, 9, 4]))
+        print(sort(second_string))
+        if first_string != second_string:
+            raise TypeError
+        if first_string in second_string:
+            return True
     except TypeError:
         return False
